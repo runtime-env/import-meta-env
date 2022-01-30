5fbe3b56b971e378c6940ba5dfeec443b8d1dfd9
Ernest <iendeavor.github.io@gmail.com>
2022-01-30 18:20:42 +0800

    feat: force split env chunk

--- packages/plugin-vite/src/index.ts
+++ packages/plugin-vite/src/index.ts
@@ -6,97 +6,61 @@ export interface PluginOptions {
 }
 
 interface NormalizedPluginOptions {
-  source: string;
   placeholder: string;
 }
-const createPlugin: (pluginOptions: PluginOptions) => Plugin = (
-  pluginOptions = {}
+const createPlugin: (pluginOptions?: PluginOptions) => Plugin = (
+  pluginOptions
 ) => {
   const name = "runtime-config";
 
   const normalizedPluginOptions: NormalizedPluginOptions = {
-    source: pluginOptions.source || ".env",
-    placeholder: pluginOptions.placeholder || "__VITE_RUNTIME_CONFIG__",
+    placeholder: "__VITE_RUNTIME_CONFIG__",
+    ...pluginOptions,
   };
   let config: ResolvedConfig;
   const commonPlugin: Plugin = {
     name,
-    config: () => {
-      // console.log("=== config ===");
-      return { assetsInclude: normalizedPluginOptions.source };
+    enforce: "post",
+    config() {
+      return { assetsInclude: ".env" };
     },
-    configResolved: (_config) => {
-      // console.log("=== configResolved ===", _config);
+    configResolved(_config) {
       config = _config;
     },
     resolveId(id) {
-      // console.log("=== resolveId ===", id);
-      if (id === normalizedPluginOptions.source) {
+      if (id === ".env") {
         return id;
       }
     },
   };
 
   if (process.env.NODE_ENV === "production") {
-    // console.log("=== production ===");
     return {
       ...commonPlugin,
       load(id) {
-        // console.log("=== load ===", id);
-        if (id === normalizedPluginOptions.source) {
-          const result = `export default ${normalizedPluginOptions.placeholder}`;
-          // console.log("=== result ===", result);
-          return result;
+        if (id === ".env") {
+          this.emitFile({
+            type: "asset",
+            fileName: "assets/.env",
+            source: `export default ${normalizedPluginOptions.placeholder}`,
+          });
+          return `const env = import(${JSON.stringify(
+            "/.env"
+          )}); export default env`;
         }
       },
     };
   } else {
-    // console.log("=== development ===");
     return {
       ...commonPlugin,
       load(id) {
-        // console.log("=== load ===", id);
-        if (id === "/" + normalizedPluginOptions.source) {
+        if (id === "/" + ".env") {
           const result = `export default ${JSON.stringify(config.env)}`;
-          // console.log("=== result ===", result);
           return result;
         }
-      },
-      configureServer(server) {
-        server.watcher.add(normalizedPluginOptions.source);
       },
     };
   }
 };
 
 export default createPlugin;
-
-// options: (options) => {
-//   return {
-//     ...options,
-//     external: (source, ...restArgs) => {
-// console.log("external", source);
-//       const optionExternal = options.external;
-//       let normalizedOptionExternal: () => boolean = () => false;
-//       if (typeof optionExternal === "string") {
-//         normalizedOptionExternal = () => optionExternal === source;
-//       } else if (optionExternal instanceof RegExp) {
-//         normalizedOptionExternal = () => optionExternal.test(source);
-//       } else if (typeof optionExternal === "function") {
-//         normalizedOptionExternal = () =>
-//           optionExternal(source, ...restArgs) || false;
-//       }
-//       if (normalizedOptionExternal()) {
-//         return true;
-//       }
-//       if (process.env.NODE_ENV === "production") {
-//         return source === normalizedPluginOptions.source;
-//       }
-//     },
-//   };
-// },
-// generateBundle: (options, bundle, isWrite) => {
-// console.log("bundle");
-// console.log(bundle);
-// console.log("");
-// },
