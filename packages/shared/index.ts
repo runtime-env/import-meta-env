562ce606a36f958782f430d4a19ba0f5f8d56da9
Ernest <iendeavor.github.io@gmail.com>
2022-01-30 22:34:14 +0800

    refactor: remove unused code

--- packages/shared/index.ts
+++
@@ -1,5 +0,0 @@
-export const key = "__runtime_config__";
-
-const scriptTemplateBegin = `<!--${key} BEGIN-->`;
-const scriptTemplateEnd = `<!--${key} END-->`;
-export const scriptTemplate = [scriptTemplateBegin, scriptTemplateEnd].join("");
