7bd23204c14b8fb6d403b23213f0bb1c2c737df9
Ernest <iendeavor.github.io@gmail.com>
2022-01-31 12:47:28 +0800

    feat: inject before preview

--- packages/examples/vite-vanilla-ts/inject-env.sh
+++ packages/examples/vite-vanilla-ts/inject-env.sh
@@ -1,1 +1,37 @@
-find ./dist/assets/.env* -type f -exec sed -i '' 's/__RUNTIME_CONFIG__/{VITE_NAME:"World"}/g' {} \;
+# construct env json from .env
+ENV_JSON=""
+ENV_JSON+="{"
+while read line; do
+  KEY_VALUE_PAIR=(`echo $line | sed 's/=/\n/g'`)
+  if [ ${KEY_VALUE_PAIR[0]} ]
+  then
+    ENV_JSON+=${KEY_VALUE_PAIR[0]}
+    ENV_JSON+=":\""
+    ENV_JSON+=${KEY_VALUE_PAIR[1]}
+    ENV_JSON+="\","
+  fi
+done < .env
+ENV_JSON=${ENV_JSON%,*}
+ENV_JSON+="}"
+
+# inject env json to .env.js
+if [ -e ./dist/assets/.env.js~ ]
+then
+  # restore env
+  cp ./dist/assets/.env.js~ ./dist/assets/.env.js
+else
+  # backup env
+  cp ./dist/assets/.env.js ./dist/assets/.env.js~
+fi
+sed -i '' "s/__RUNTIME_CONFIG__/$ENV_JSON/g" ./dist/assets/.env.js;
+
+if [ -e ./dist/assets/.env-legacy.js ]
+then
+  if [ -e ./dist/assets/.env-legacy.js~ ]
+  then
+    cp ./dist/assets/.env-legacy.js~ ./dist/assets/.env-legacy.js
+  else
+    cp ./dist/assets/.env-legacy.js ./dist/assets/.env-legacy.js~
+  fi
+  sed -i '' "s/__RUNTIME_CONFIG__/$ENV_JSON/g" ./dist/assets/.env-legacy.js;
+fi
