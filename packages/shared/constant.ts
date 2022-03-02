82e0b9547f2f745a9e9fbecdd006752f0179b52d
Ernest <iendeavor.github.io@gmail.com>
2022-03-02 11:12:43 +0800

    refactor: remove unused variable

--- packages/shared/constant.ts
+++ packages/shared/constant.ts
@@ -1,7 +1,5 @@
 export const virtualFile = "import-meta-env";
 
-export const virtualId = "\0" + virtualFile;
-
 export const placeholder = "__import_meta_env_placeholder__";
 
 export const uniqueVariableName = (() => {
