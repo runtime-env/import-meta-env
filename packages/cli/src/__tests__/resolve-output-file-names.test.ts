f1a5e1e6e873074fecab8674d5f1fe57fca62e0d
Ernest <iendeavor.github.io@gmail.com>
2022-03-04 16:00:58 +0800

    test: add tests

--- packages/cli/src/__tests__/resolve-output-file-names.test.ts
+++ packages/cli/src/__tests__/resolve-output-file-names.test.ts
@@ -1,4 +1,5 @@
 import { writeFileSync, mkdirSync } from "fs";
+import { resolve } from "path";
 import tmp from "tmp";
 import { resolveOutputFileNames } from "../resolve-output-file-names";
 
@@ -15,47 +16,67 @@ describe("resolveOutputFileNames", () => {
     expect(act).not.toThrow();
   });
 
+  test("it should call glob.sync", () => {
+    // arrange
+    const tmpDir = tmp.dirSync();
+    writeFileSync(resolve(tmpDir.name, "foo"), "", "utf8");
+    const tmpDirGlob = resolve(tmpDir.name, "**", "*");
+    const spy = jest.spyOn(require("glob"), "sync");
+
+    // act
+    resolveOutputFileNames([tmpDirGlob]);
+
+    // assert
+    expect(spy).toHaveBeenCalledWith(tmpDirGlob);
+  });
+
   test("it should return file names in multiple globs", () => {
     // arrange
     const tmpDir1 = tmp.dirSync();
-    writeFileSync(`${tmpDir1.name}/foo`, "", "utf8");
+    writeFileSync(resolve(tmpDir1.name, "foo"), "", "utf8");
     const tmpDir2 = tmp.dirSync();
-    writeFileSync(`${tmpDir2.name}/bar`, "", "utf8");
+    writeFileSync(resolve(tmpDir2.name, "bar"), "", "utf8");
 
     // act
     const result = resolveOutputFileNames([
-      tmpDir1.name + "/*",
-      tmpDir2.name + "/*",
+      resolve(tmpDir1.name, "*"),
+      resolve(tmpDir2.name, "*"),
     ]);
 
     // assert
-    expect(result).toEqual([`${tmpDir1.name}/foo`, `${tmpDir2.name}/bar`]);
+    expect(result).toEqual([
+      resolve(tmpDir1.name, "foo"),
+      resolve(tmpDir2.name, "bar"),
+    ]);
   });
 
   test("it should return file names only", () => {
     // arrange
     const tmpDir = tmp.dirSync();
-    writeFileSync(`${tmpDir.name}/foo`, "", "utf8");
-    mkdirSync(`${tmpDir.name}/bar`);
+    writeFileSync(resolve(tmpDir.name, "foo"), "", "utf8");
+    mkdirSync(resolve(tmpDir.name, "bar"));
 
     // act
-    const result = resolveOutputFileNames([tmpDir.name + "/*"]);
+    const result = resolveOutputFileNames([resolve(tmpDir.name, "*")]);
 
     // assert
-    expect(result).toEqual([`${tmpDir.name}/foo`]);
+    expect(result).toEqual([resolve(tmpDir.name, "foo")]);
   });
 
   test("it should return nested file names", () => {
     // arrange
     const tmpDir = tmp.dirSync();
-    writeFileSync(`${tmpDir.name}/foo`, "", "utf8");
-    mkdirSync(`${tmpDir.name}/bar`);
-    writeFileSync(`${tmpDir.name}/bar/baz`, "", "utf8");
+    writeFileSync(resolve(tmpDir.name, "foo"), "", "utf8");
+    mkdirSync(resolve(tmpDir.name, "bar"));
+    writeFileSync(resolve(tmpDir.name, "bar", "baz"), "", "utf8");
 
     // act
-    const result = resolveOutputFileNames([tmpDir.name + "/**/*"]);
+    const result = resolveOutputFileNames([resolve(tmpDir.name, "**", "*")]);
 
     // assert
-    expect(result).toEqual([`${tmpDir.name}/bar/baz`, `${tmpDir.name}/foo`]);
+    expect(result).toEqual([
+      resolve(tmpDir.name, "bar", "baz"),
+      resolve(tmpDir.name, "foo"),
+    ]);
   });
 });
