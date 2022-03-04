97488b06b771171b219478da16721ae893295952
Ernest <iendeavor.github.io@gmail.com>
2022-03-04 17:18:39 +0800

    fix: use windows native sep

--- packages/cli/src/resolve-output-file-names.ts
+++ packages/cli/src/resolve-output-file-names.ts
@@ -1,10 +1,12 @@
 import glob from "glob";
 import { existsSync, lstatSync } from "fs";
+import { resolve } from "path";
 
 export const resolveOutputFileNames = (outputPaths: string[]) => {
   return outputPaths
     .map((path) => glob.sync(path))
     .flat()
     .filter((path) => existsSync(path))
-    .filter((path) => lstatSync(path).isFile());
+    .filter((path) => lstatSync(path).isFile())
+    .map((path) => resolve(path));
 };
