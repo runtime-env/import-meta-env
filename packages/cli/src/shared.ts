74f9fc03fb28b7231d07788e215088b21613a7d6
Ernest <iendeavor.github.io@gmail.com>
2022-03-04 16:07:19 +0800

    chore: add comments

--- packages/cli/src/shared.ts
+++ packages/cli/src/shared.ts
@@ -1,9 +1,14 @@
 export const backupFileExt = ".bak";
 
 export const defaultOutput = [
+  // vite, webpack, vue-cli, nuxt 2 static-site
   "dist/**/*",
+  // create-next-app
   ".next/**/*",
+  // create-nuxt-app
   ".nuxt/**/*",
+  // nuxt bridge
   ".output/**/*",
+  // create-react-app
   "build/**/*",
 ];
