3bcaff12f543c44bf5463f1c175bf62e79531690
Ernest <iendeavor.github.io@gmail.com>
2022-03-09 09:03:12 +0800

    docs: init docs

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vitepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:iendeavor/import-meta-env.git main:gh-pages
+
+cd -
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  base: "/import-meta-env/",
+});
