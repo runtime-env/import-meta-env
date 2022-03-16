979b8f46f367c1b9b34de77ef74e1d4ac7307b6f
Ernest <iendeavor.github.io@gmail.com>
2022-03-16 14:47:41 +0800

    test: fix wrong fixture

--- packages/examples/svelte-kit-example/__fixtures__/import-meta-env-a68611e7.js
+++ packages/examples/svelte-kit-example/__fixtures__/import-meta-env-a68611e7.js
@@ -1,3 +1,3 @@
 console.assert("d3612f0e3785495502e39a5f98aae571e7b0e53f");
-const e = Object.assign({"HELLO":"import-meta-env"}, { "VITE_SVELTEKIT_APP_VERSION_FILE": "_app/version.json", "VITE_SVELTEKIT_APP_VERSION_POLL_INTERVAL": "0", "VITE_SVELTEKIT_AMP": "", "BASE_URL": "/_app/", "MODE": "production", "DEV": false, "PROD": true });
+const e = Object.assign({"HELLO":"import-meta-env"}, { "VITE_SVELTEKIT_APP_VERSION_FILE": "_app/version.json", "VITE_SVELTEKIT_APP_VERSION_POLL_INTERVAL": "0", "VITE_SVELTEKIT_AMP": "", "VITE_SVELTEKIT_ADAPTER_NAME": "@sveltejs/adapter-auto", "BASE_URL": "/_app/", "MODE": "production", "DEV": false, "PROD": true });
 export { e };
