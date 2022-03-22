1e5ad05f0affd47944b02d01973fa2edf62014ca
Ernest <iendeavor.github.io@gmail.com>
2022-03-22 17:38:21 +0800

    docs: remove hello-world example (use webpack instead)

--- packages/examples/hello-world-example/src/index.js
+++
@@ -1,1 +0,0 @@
-console.log(`Hello: ${import.meta.env.HELLO}`);
--- packages/examples/hello-world-example/webpack.config.js
+++
@@ -1,5 +0,0 @@
-const importMetaEnv = require("@import-meta-env/unplugin");
-
-module.exports = {
-  plugins: [importMetaEnv.webpack({ example: ".env.example.public" })],
-};
