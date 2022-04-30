285b662977f41d65e26203548e3e5eb0786d28d5
Ernest <iendeavor.github.io@gmail.com>
2022-04-30 12:54:13 +0800

    test: to prevent using same port

--- packages/examples/compression-example/test.js
+++ packages/examples/compression-example/test.js
@@ -5,9 +5,9 @@ const commands = [
   "pnpm exec webpack",
   "pnpm exec cross-env HELLO=foo pnpm exec import-meta-env --example .env.example.public --compression compression.js",
 ];
-const longRunningCommands = ["pnpm -w serve -- -d dist -p 4198 --br"];
+const longRunningCommands = ["pnpm -w serve -- -d dist -p 4203 --br"];
 const expected = "Hello: foo";
-const url = "http://localhost:4198";
+const url = "http://localhost:4203";
 const waitMs = 1000;
 
 runTest({
