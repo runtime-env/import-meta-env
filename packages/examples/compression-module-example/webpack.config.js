5678045892681ce41f1f5f6192d107cf1e7c978d
Ernest <iendeavor.github.io@gmail.com>
2022-05-06 19:36:22 +0800

    docs: fix development mode example

--- packages/examples/compression-module-example/webpack.config.js
+++ packages/examples/compression-module-example/webpack.config.js
@@ -5,15 +5,33 @@ const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
       inject: false,
+      templateContent: `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Webpack App</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+  </head>
+  <body>
+    <script src="main.js${
+      process.env.NODE_ENV === "development" ? "" : ".br"
+    }"></script>
+  </body>
+</html>
+    `.trim(),
     }),
-    new CompressionPlugin({
-      algorithm: "brotliCompress",
-      filename: "[path][base].br",
-      test: /\.js$/,
-      deleteOriginalAssets: true,
-    }),
+    ...(process.env.NODE_ENV === "development"
+      ? []
+      : [
+          new CompressionPlugin({
+            algorithm: "brotliCompress",
+            filename: "[path][base].br",
+            test: /\.js$/,
+            deleteOriginalAssets: true,
+          }),
+        ]),
     importMetaEnv.webpack({ example: ".env.example.public" }),
   ],
   optimization: {
