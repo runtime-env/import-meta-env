30e6e4604a119300dec488b7b36b8b88da7dd014
Ernest <iendeavor.github.io@gmail.com>
2022-10-07 22:43:42 +0800

    docs: update alpine.js example

--- packages/examples/vite-alpine-example/src/main.js
+++ packages/examples/vite-alpine-example/src/main.js
@@ -1,7 +1,9 @@
 import Alpine from "alpinejs";
 
-Alpine.store("env", {
-  HELLO: import.meta.env.HELLO,
+Alpine.store("import", {
+  meta: {
+    env: import.meta.env,
+  },
 });
 
 Alpine.start();
