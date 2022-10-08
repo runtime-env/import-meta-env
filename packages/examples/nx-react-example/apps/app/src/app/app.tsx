e4defac25c89ff1bfd71ec478713720bd82d85d4
Ernest <iendeavor.github.io@gmail.com>
2022-10-08 23:40:47 +0800

    docs: add nx react example

---
+++ packages/examples/nx-react-example/apps/app/src/app/app.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+export function App() {
+  return <div>Hello: {import.meta.env.HELLO}</div>;
+}
+
+export default App;
