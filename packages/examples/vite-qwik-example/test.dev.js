15838ec18cb90dfd162e3d778c9210743ac71b44
Ernest <iendeavor.github.io@gmail.com>
2022-10-09 21:03:41 +0800

    docs: update qwik example

---
+++ packages/examples/vite-qwik-example/test.dev.js
@@ -0,0 +1,27 @@
+const runTest = require("../run-test");
+const getPort = require("../get-port");
+
+module.exports = async () => {
+  const port = await getPort();
+  const hello = Math.random();
+
+  const commands = [
+    "npx rimraf dist node_modules/.vite",
+    "npm add ../../unplugin/import-meta-env-unplugin-test.tgz",
+  ];
+  const longRunningCommands = [
+    `npx cross-env HELLO=${hello} vite dev --mode ssr --port ${port}`,
+  ];
+  const expected = `Hello: ${hello}`;
+  const url = `http://localhost:${port}`;
+  const waitMs = 2000;
+
+  await runTest({
+    commands,
+    longRunningCommands,
+    expected,
+    url,
+    waitMs,
+    noExit: true,
+  });
+};
