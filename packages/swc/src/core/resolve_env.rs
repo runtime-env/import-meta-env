10ee301d783312baf56f4cfa5c1dbc4e157f6e0c
Ernest <iendeavor.github.io@gmail.com>
2022-11-16 21:40:55 +0800

    fix: show helpful error message

--- packages/swc/src/core/resolve_env.rs
+++ packages/swc/src/core/resolve_env.rs
@@ -4,11 +4,15 @@ pub fn resolve_env(
     env_file_name: Option<String>,
     env_example_file_name: String,
 ) -> Vec<(String, String)> {
-    let env: Vec<(String, String)> = if let Some(env_file_name) = env_file_name {
-        resolve_env_from_file_name(&env_file_name)
-    } else {
-        resolve_env_from_file_name(".env")
+    let normalized_env_file_name = match env_file_name {
+        Some(env_file_name) => env_file_name,
+        None => String::from(".env"),
     };
+    let env: Vec<(String, String)> = resolve_env_from_file_name(&normalized_env_file_name);
+    let env_keys = env
+        .iter()
+        .map(|(k, _)| k.to_owned())
+        .collect::<Vec<String>>();
     let env_example = resolve_env_from_file_name(&env_example_file_name);
     let env_example_keys = env_example
         .iter()
@@ -19,6 +23,45 @@ pub fn resolve_env(
         .into_iter()
         .filter(|(k, _)| env_example_keys.contains(&k))
         .collect();
+
+    let missing_keys: Vec<String> = env_example
+        .iter()
+        .filter(|(k, _)| env_keys.contains(&k) == false)
+        .map(|(k, _)| k.to_owned())
+        .collect();
+    if missing_keys.len() != 0 {
+        println!(
+            r#"
+[import-meta-env]: Some environment variables are not defined.
+
+The following variables were defined in {0} file but are not defined in the environment:
+
+```
+{2}
+```
+
+Here's what you can do:
+- Set them to environment variables on your system.
+- Add them to {1} file.
+- Remove them from {0} file.
+        "#,
+            env_example_file_name,
+            normalized_env_file_name,
+            missing_keys
+                .iter()
+                .map(|key| key.to_owned()
+                    + "="
+                    + env_example
+                        .iter()
+                        .find(|(k, _)| k == key)
+                        .unwrap()
+                        .1
+                        .as_str())
+                .collect::<Vec<String>>()
+                .join("\n")
+        );
+        panic!("Some environment variables are not defined.");
+    }
 
     filtered_env.sort();
 
