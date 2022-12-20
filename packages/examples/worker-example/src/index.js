0a42aec6eb63d6663b133581ab075dfb455cc463
Ernest <iendeavor.github.io@gmail.com>
2022-12-20 20:16:25 +0800

    docs: update

--- packages/examples/worker-example/src/index.js
+++ packages/examples/worker-example/src/index.js
@@ -1,4 +1,4 @@
-const worker = new Worker(new URL("./deep-thought.js", import.meta.url));
+const worker = new Worker(new URL("./worker.js", import.meta.url));
 
 worker.onmessage = ({ data }) => {
   document.querySelector("body").innerHTML = `
