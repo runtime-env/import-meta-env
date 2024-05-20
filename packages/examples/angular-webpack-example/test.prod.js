450003b5d28738bae42edfd9fa70de67b367cb52
Ernest <22850435+soc221b@users.noreply.github.com>
2024-05-20 20:55:26 +0800

    docs: bump angular example (#1249)
    Co-authored-by: Ernest <iendeavor.github.io@gmail.com>

--- packages/examples/angular-webpack-example/test.prod.js
+++ packages/examples/angular-webpack-example/test.prod.js
@@ -12,7 +12,7 @@ module.exports = async () => {
     "npx ng build",
     `npx cross-env MODE=production HELLO=${hello} npx import-meta-env -x .env.example.public`,
   ];
-  const longRunningCommands = [`node ../serve.js -d dist/angular-example -p ${port}`];
+  const longRunningCommands = [`node ../serve.js -d dist/angular-webpack-example -p ${port}`];
   const expected = `Hello: ${hello}`;
   const url = `http://localhost:${port}`;
   const waitMs = 2000;
