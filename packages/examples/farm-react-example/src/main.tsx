465a30e063fc3a6a199d9c019c44675440158ecf
Ernest <22850435+soc221b@users.noreply.github.com>
2024-07-20 17:46:29 +0800

    fix: farm (#1347)
    Co-authored-by: Ernest <>

---
+++ packages/examples/farm-react-example/src/main.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export function Main() {
+  return <h1>Hello: {import.meta.env.HELLO}</h1>;
+}
---
+++ packages/examples/farm-react-example/test.prod.js
@@ -0,0 +1,28 @@
+const runTest = require("../run-test");
+const getPort = require("../get-port");
+
+module.exports = async () => {
+  const port = await getPort();
+  const hello = Math.random();
+
+  const commands = [
+    "npx rimraf dist node_modules/.farm",
+    "npm add ../../cli/import-meta-env-cli-test.tgz",
+    "npm add ../../unplugin/import-meta-env-unplugin-test.tgz",
+    "npx farm build",
+    `npx cross-env HELLO=${hello} npx import-meta-env -x .env.example.public`,
+  ];
+  const longRunningCommands = [`npx farm preview --port ${port}`];
+  const expected = `Hello: ${hello}`;
+  const url = `http://localhost:${port}`;
+  const waitMs = 1000;
+
+  await runTest({
+    commands,
+    longRunningCommands,
+    expected,
+    url,
+    waitMs,
+    noExit: true,
+  });
+};
