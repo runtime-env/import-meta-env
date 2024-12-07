8271bb6e9b16c098ef61378951b76e0c4074b7c2
Ernest <soc221b.e@gmail.com>
2024-12-07 17:27:01 +0800

    docs: update example

--- packages/examples/process-env-example/pages/api/process-env-secret2.js
+++ packages/examples/process-env-example/pages/api/process-env-secret2.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json(JSON.stringify(process.env.SECRET2));
+  res.status(200).json(process.env.SECRET2);
 }
