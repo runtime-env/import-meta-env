eb36709f71d5c3e0ac460618a71141d5722989b0
Ernest <soc221b.e@gmail.com>
2025-02-16 15:21:11 +0800

    chore: migrate to nx@16

--- packages/examples/nx-react-example/apps/app/jest.config.ts
+++ packages/examples/nx-react-example/apps/app/jest.config.ts
@@ -3,11 +3,11 @@ export default {
   displayName: 'app',
   preset: '../../jest.preset.js',
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': [
       'babel-jest',
       {
-        presets: ['@nrwl/react/babel'],
+        presets: ['@nx/react/babel'],
         plugins: [
           ['module:@import-meta-env/babel', { example: '.env.example.public' }],
         ],
