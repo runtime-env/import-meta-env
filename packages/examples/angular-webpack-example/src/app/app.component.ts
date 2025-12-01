4704466f246c7ced258e15801dbc1a3bcd2a5860
Ernest <soc221b.e@gmail.com>
2025-12-01 14:40:38 +0800

    chore(deps): update angular-cli monorepo to v19 (major)

--- packages/examples/angular-webpack-example/src/app/app.component.ts
+++ packages/examples/angular-webpack-example/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { environment } from './../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent {
   hello = environment.hello;
